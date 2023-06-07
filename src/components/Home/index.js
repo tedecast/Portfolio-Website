86e6d059cdbf12035bde682ab9e467b3edd380a3
Teresa de Castro <decastro.ter@gmail.com>
2023-06-07 17:12:35 -0400

    Made adjustments, prior to adding in hamburger menu for mobile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,12 +7,12 @@ const Home = () => {
       <div className="container home-page">
         <div className="text-zone">
           <h1 className="eresa">
-            Hi, <br /> I'm
+            Hi, <br/>I'm
             <span className="t-logo">T</span>
             <span className="eresa">eresa</span>
             <br />
           </h1>
-          <h1>Full-Stack Developer</h1>
+          <h2>Full-Stack Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
