b3f25029f2509a74e7bc4d368bab783f593059a8
Teresa de Castro <decastro.ter@gmail.com>
2023-06-07 15:04:12 -0400

    Started fonts on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
 const Home = () => {
   return (
     <>
-      <div className="container home-page">Hello</div>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            Hi, <br /> I'm
+            <span className="t-logo">T</span><span>eresa</span>
+            <br />
+          </h1>
+          <h2>Full-Stack Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
