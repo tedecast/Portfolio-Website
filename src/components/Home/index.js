120bd16efbcb7d34f5c58c5555f2c3294be3fffc
Teresa de Castro <decastro.ter@gmail.com>
2023-06-08 16:39:27 -0400

    Managed to get erri to move

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 
 const Home = () => {
   const emoji = '\u{2728}'
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['e', 'r', 'r', 'i']
+
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1 className="landing-text">
-            Hi, <br />
-            I'm
-            &nbsp;
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I'</span>
+            <span className={`${letterClass} _14`}>m</span>
             <span className="t-logo">T</span>
-            <span className='erri'>erri</span>
-            <span className='emoji'>{emoji}</span>
+            {nameArray.map((letter, index) => (
+              <span
+                key={index}
+                className={`${letterClass} erri _${index + 15}`}
+              >
+                {letter}
+              </span>
+            ))}
+            <span className="emoji">{emoji}</span>
             <br />
           </h1>
           <h2>Web Developer</h2>
